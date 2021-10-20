6b348532081053101485357a780797666b09db32
Joe Clinton <joeclinton1@btinternet.com>
2021-10-20 20:11:51 +0100

    Fix preview-files not working after navigation

--- js/addons/preview-files.js
+++ js/addons/preview-files.js
@@ -1,12 +1,30 @@
-onExists(".file-container-panel", ()=>{
-    const file_container_panels = document.querySelectorAll(".file-container-panel")
-    file_container_panels.forEach((file_container_panel) => {
-        const previewLink = file_container_panel.querySelector(".file-preview a").getAttribute("data-ally-file-preview-url");
-        const filenameSpan = file_container_panel.querySelector("span.file-name");
-        filenameSpan.innerHTML = `
-            <a href = "${previewLink}">
-                ${filenameSpan.innerHTML}
-            </a>
-        `;
+replaceLinks();
+
+function replaceLinks(){
+    onExists(".content-navigator-container",()=>{
+        console.log(document.querySelectorAll(".content-navigator-container a"))
+        document.querySelectorAll(".content-navigator-container a").forEach((button)=>{
+            console.log("button");
+            button.addEventListener("click", ()=>{
+                console.log("clicked")
+                onNotExists(".file-container-panel", replaceLinks, 50)
+            });
+        })
     });
-})
+    
+    onExists(".file-container-panel", ()=>{
+        const file_container_panels = document.querySelectorAll(".file-container-panel")
+        file_container_panels.forEach((file_container_panel) => {
+            const previewLink = file_container_panel.querySelector(".file-preview a").getAttribute("data-ally-file-preview-url");
+            const filenameSpan = file_container_panel.querySelector("span.file-name");
+            filenameSpan.innerHTML = `
+                <a href = "${previewLink}">
+                    ${filenameSpan.innerHTML}
+                </a>
+            `;
+        });
+    });
+}
+
+
+
--- js/utils.js
+++ js/utils.js
@@ -2,8 +2,21 @@ function onExists(selector, callback, interval = 100){
     var checkExists = setInterval(function() {
         el = document.querySelector(selector)
         if (el != null) {
-           clearInterval(checkExists);
-           callback();
+            console.log("success: element found")
+            clearInterval(checkExists);
+            callback();
+        }
+    }, interval);
+}
+
+function onNotExists(selector, callback, interval = 100){
+    var checkNotExists = setInterval(function() {
+        el = document.querySelector(selector)
+        console.log(el)
+        if (el == null) {
+            console.log("success: element not found")
+            clearInterval(checkNotExists);
+            callback();
         }
     }, interval);
 }
