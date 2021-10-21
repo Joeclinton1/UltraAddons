5955d90c81aca648b91692f0cffaaef47ba91845
Joe Clinton <joeclinton1@btinternet.com>
2021-10-21 03:11:19 +0100

    Add support for bb editor

--- js/addons/preview-files.js
+++ js/addons/preview-files.js
@@ -4,26 +4,30 @@ const callback = function(mutationsList, observer) {
     // Use traditional 'for loops' for IE 11
     for(const mutation of mutationsList) {
         if (mutation.addedNodes && mutation.addedNodes.length > 0) {
-            Array.from(mutation.addedNodes).some((node)=>{
+            Array.from(mutation.addedNodes).forEach((node)=>{
                 if(node.classList && node.classList.contains('panel-content-inner')){
-                    addPreviewLink(mutation.target)
-                    return true;
+                    addPreviewLinkFileContainer(node)
+                }else if(node.classList && node.classList.contains('wants-props-update')){
+                    addPreviewLinkBBEditor(node)
                 }
             })
         }
     }
 };
 
-function addPreviewLink(container){
+function addPreviewLinkFileContainer(container){
     container.querySelectorAll(".file-container").forEach((file_container)=>{
         const previewLink = file_container.querySelector(".file-preview a").getAttribute("data-ally-file-preview-url");
         const filenameSpan = file_container.querySelector("span.file-name");
-        filenameSpan.innerHTML = `
-            <a href = "${previewLink}">
-                ${filenameSpan.innerHTML}
-            </a>
-        `;
+        filenameSpan.outerHTML = `<a href = "${previewLink}">${filenameSpan.outerHTML}</a>`;
     })
+}
+
+function addPreviewLinkBBEditor(file_container){
+    const previewLink = file_container.getAttribute("href");
+    const filenameSpan = file_container.querySelector("span.bb-file-viewer-file-text");
+    console.log(filenameSpan.innerText);
+    filenameSpan.outerHTML = `<a href = "${previewLink}">${filenameSpan.outerHTML}</a>`;;
 }
 
 const observer = new MutationObserver(callback);
